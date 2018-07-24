ea7cd3403e5b5bf0212e6658bcfc1d97e57304a8
ltttruc4th2 <ltttruc4th2@gmail.com>
2018-07-25 02:23:28 +0700

    Add some functions: currentURL, open new tab, change size of browser

--- SampleFirefoxSelenium/src/Sample.java
+++ SampleFirefoxSelenium/src/Sample.java
@@ -1,21 +1,57 @@
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.interactions.Actions;
 
 public class Sample {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.gecko.driver", "/Users/macbook/eclipse-workspace/SampleFirefoxSelenium/lib/geckodriver/geckodriver");
+		System.setProperty("webdriver.gecko.driver", "/Users/macbook/git/Project25July/SampleFirefoxSelenium/lib/geckodriver/geckodriver");
 
-		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-		capabilities.setCapability("marionette", true);
-		
 		
 		try {
 			
 			WebDriver driver = new FirefoxDriver();
-			driver.get("https://www.google.com");
+			
+			
+			//Always maximize the browser window
+			driver.manage().window().maximize();
+			
+			//Go to seleniumhq page
+			driver.get("http://seleniumhq.org/");
+			
+			//Get current URL
+			String currentURL = driver.getCurrentUrl();
+			System.out.println(currentURL);
 			Thread.sleep(1800);
+			
+			//Open new tab and go to google.com
+			if (driver instanceof JavascriptExecutor) {
+		        ((JavascriptExecutor)driver).executeScript("window.open();");
+		        }
+			ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+	        driver.switchTo().window(tabs.get(1));
+	        Thread.sleep(1800);
+			//driver.findElement(By.cssSelector("Body")).sendKeys(Keys.chord(Keys.COMMAND,"t"));
+			driver.get("https://google.com");
+			
+			
+			//Get current size of window and set the dimension to 640 x 480
+			System.out.println(driver.manage().window().getSize());
+			Dimension d = new Dimension(640, 480);
+			
+			driver.manage().window().setSize(d);
+			System.out.println(driver.manage().window().getSize());
+			
+			//Sleep for 1.8s
+			Thread.sleep(1800);
+			
+			//Quite Firefox
 			driver.quit();
 			
 		} catch (Exception e) {
@@ -24,5 +60,7 @@ public class Sample {
 		}
 		
 	}
+	
+	
 
 }
