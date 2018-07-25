9c43d762f6b8ed797d04f297b52e12dec55f051d
ltttruc4th2 <ltttruc4th2@gmail.com>
2018-07-25 17:54:20 +0700

    Capture screenshot in assertTitle

--- SampleFirefoxSelenium/src/assertTitle.java
+++ SampleFirefoxSelenium/src/assertTitle.java
@@ -1,14 +1,18 @@
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import java.io.*;
+import org.apache.commons.io.FileUtils;
 
 
 public class assertTitle {
 
 	public static void main(String[] args) {
 
-		String driverPath = System.getProperty("usr.dir");
-		System.setProperty("webdriver.gecko.driver", driverPath + "/lib/gecko/geckodriver");
+		//String driverPath = System.getProperty("usr.dir");
+		System.setProperty("webdriver.gecko.driver", "/Users/macbook/git/Project25July/SampleFirefoxSelenium/lib/gecko/geckodriver");
 		WebDriver driver = new FirefoxDriver();
 		
 		try {
@@ -25,11 +29,15 @@ public class assertTitle {
 			
 			Assert.assertTrue(title.contains("Google"));
 			
-			Thread.sleep(1500);
+			//capture picture
+			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 			
+			FileUtils.copyFile(scrFile, new File ("/Users/macbook/git/Project25July/SampleFirefoxSelenium/output/o1.png"));
+			
+			Thread.sleep(1500);
 			driver.quit();
 			
-		} catch (InterruptedException e) {
+		} catch (InterruptedException | IOException e) {
 			e.printStackTrace();
 		}
 		
